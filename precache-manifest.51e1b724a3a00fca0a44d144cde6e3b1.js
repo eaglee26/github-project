b44567fb1e0145dd2ea2b4bc4490938ab53b6549
Sergio <sergiiy@tallium.com>
2020-06-07 17:34:11 +0300

    Updates

---
+++ precache-manifest.51e1b724a3a00fca0a44d144cde6e3b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb111941319dcded36f1d6f16b807547",
+    "url": "/github-project/index.html"
+  },
+  {
+    "revision": "d10ed05dbbe5978efee6",
+    "url": "/github-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb9f28736dfb633d4ea6",
+    "url": "/github-project/static/js/2.5aa9bcaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-project/static/js/2.5aa9bcaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10ed05dbbe5978efee6",
+    "url": "/github-project/static/js/main.25a9e544.chunk.js"
+  },
+  {
+    "revision": "fd868af84871456b38db",
+    "url": "/github-project/static/js/runtime-main.1296bb94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
